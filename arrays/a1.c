2adc7fc7d129726c955268916603f0a917a0d581
Vishal <vishalmaurya12nov@gmail.com>
2022-01-23 09:24:24 -0500

    commit

---
+++ arrays/a1.c
@@ -0,0 +1,13 @@
+// using loop array manipulation
+#include <stdio.h>
+int main()
+{
+    int n[10], i;
+    printf("enter array element\n");
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &n[i]);
+    }
+    for (i = 0; i <= 9; i++)
+        printf("n[%d]=%d\n", i, n[i]);
+}
